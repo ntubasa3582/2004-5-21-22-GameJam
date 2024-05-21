4342b7195ca1312a69fb06764ea275cb09935683
nagawatomo <tomonagawa@outlook.jp>
2024-05-21 14:00:38 +0900

    [add][update]画像の追加　紫陽花のランダム選択

--- Assets/Nakagawa/Azisai.cs
+++ Assets/Nakagawa/Azisai.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Azisai : MonoBehaviour
 {
     [SerializeField] private int _bloomCount;
+    [SerializeField] private Sprite[] _azisaiImages = new Sprite[9];
 
     private int _count;
     private bool _isBlooming;
+    private SpriteRenderer _renderer;
 
     public bool IsBlooming => _isBlooming;
 
+    private void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (_isBlooming) { return; }
@@ -21,6 +28,7 @@ public class Azisai : MonoBehaviour
             if (_count >= _bloomCount)
             {
                 Debug.Log("çÁÇ¢ÇΩ");
+                _renderer.sprite = _azisaiImages[Random.Range(0,_azisaiImages.Length)] ;
                 _isBlooming = true;
             }
         }
