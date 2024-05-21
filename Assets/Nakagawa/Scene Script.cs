08430f9c58a8fdf547912987b341c2bf11e12f4e
nagawatomo <tomonagawa@outlook.jp>
2024-05-21 16:58:15 +0900

    [add] SceneScript作成

---
+++ Assets/Nakagawa/Scene Script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+    static SceneScript instance = default;
+    public static SceneScript Instance => instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = GetComponent<SceneScript>();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
