89b05bf77f9f15570fd6081aee3ee589a1ec715b
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2024-05-21 17:50:47 +0900

    マージシーンの調整

--- Assets/Nakagawa/Scene Script.cs
+++ Assets/Nakagawa/Scene Script.cs
@@ -25,6 +25,6 @@ public class SceneScript : MonoBehaviour
 
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+            SceneManager.LoadScene(sceneName);
     }
 }
